16c02fbd41c3e59ad884420da10c5d33cce4e408
Carlosmiguel22 <cm9062643@gmail.com>
2025-09-29 10:39:10 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"O Pacheco é o palhaço da turma/n";
+    std::cout<<"O Pacheco é tão feio, tão feio que quando foi fazer uma plástica para te pores mais bonito devolveram-te o dinheiro.";
+
+    return 0;
+}
