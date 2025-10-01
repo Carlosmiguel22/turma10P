7987e0acfe37c8944648d076b723d1d7f90f1674
Carlosmiguel22 <cm9062643@gmail.com>
2025-10-01 11:45:33 +0100

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    float P1
+    std::cout<<"escreve um número";
+    std::cin>>P1;
+    (P
+    {
+        std::cout<<"Passas-te";
+    }
+    else
+    {
+         std::cout<<"Passas-te";
+    }
+    
+    return 0;
+}
