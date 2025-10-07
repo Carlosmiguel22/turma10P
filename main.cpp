90a717682c03f208a5e358eae56c4b34b6ff1f79
Carlosmiguel22 <cm9062643@gmail.com>
2025-10-07 17:47:23 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
+#include <locale.h>
+using namespace std;
 int main()
 {
-    std::cout<<"O Pacheco é o palhaço da turma/n";
-    std::cout<<"O Pacheco é tão feio, tão feio que quando foi fazer uma plástica para te pores mais bonito devolveram-te o dinheiro.";
-
-    return 0;
+	setlocale(LC_ALL, "Portuguese");
+	int num;
+	do
+	{
+	    cout << "Escreva um número:\n";
+	    cin >> num;
+	    if (num == 0)
+	    {
+	        break;
+	    }
+	    if (num % 3 == 0)
+	    {
+	        cout << "É multiplo de 5 \n";
+	    }
+	}while (num !=0);
 }
