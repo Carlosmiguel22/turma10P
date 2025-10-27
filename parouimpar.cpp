af65424e38d38cd6915e08bf0a9740eec326c9a1
Carlosmiguel22 <cm9062643@gmail.com>
2025-10-27 09:55:03 +0000

    Add files via upload

---
+++ parouimpar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "Portuguese");
+	int num=28;
+while (num != 0)
+	{
+	cout << "Digite um número \n";
+	cin >> num;
+	if (num % 2 == 0)
+	{
+		cout << "é par\n";
+	}
+	else
+	{
+		cout << "impar\n";
+	}
+	}
+cout << "Carregaste no zero, saiste!";
+}
